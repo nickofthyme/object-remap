0bfa2be8a459fc92acec5fd66e6489bf1d0c0e3e
Wouter Deconinck <wdconinc@gmail.com>
2023-09-05 17:49:25 -0500

    feat: enable support for node16 (#15)
    - update action runtime to `node16`
    - code cleanup
    - add dist output ci check
    - fix deprecated `::set-output` usages
    
    BREAKING CHANGE: Update node runtime from `v12` to `v16`

--- clean_dist.sh
+++ clean_dist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Needed to cleanup ncc output for CRLF error
-# Note: Only runs on mac machines
-sed -i "" "s/$(printf '\r')\$//" dist/index.js
+# Note: Avoid space between -i and empty string
+# so this works on linux and mac machines
+sed -i"" "s/$(printf '\r')\$//" dist/index.js
